4b03554e5e7af63f2be8b7a2236aed97d16c271c
RLSnail <13355365693@qq.com>
2022-11-28 15:24:40 +0800

    创建了MainPage.vue，在App.vue里添加了MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <MainPage></MainPage>
   </div>
 </template>
 
 <script>
-
+import MainPage from './components/MainPage.vue';
 export default {
   name: 'App',
   components: {
+    MainPage
   }
 }
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <router-link to="/random-generator">随机数生成器</router-link>
+        <router-link to="/matrix-primary-transform">矩阵初等变换</router-link>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    name: "MainPage"
+}
+</script>
